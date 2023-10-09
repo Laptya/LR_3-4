2ac4ce8c7e88bdd0d0482b376f48a2d4137f2b56
Laptya <144488450+Laptya@users.noreply.github.com>
2023-10-09 17:30:31 +0300

    Update 2.pas

--- 2.pas
+++ 2.pas
@@ -1,10 +1,17 @@
-﻿var i:integer;
+var i:integer;
 begin
-  for i:=1 to 100 do
-    begin
-    write(sqr(i):6 );
-    if i mod 10 = 0 
-      then
-      writeln();
-    end;
+write('':6);
+for i:=1 to 10 do
+write(i:6);
+writeln;
+write(0:6);
+for i:=1 to 100 do
+begin
+write(sqr(i):6);
+if (i mod 10 = 0) and (i <> 100) then
+begin
+writeln;
+write(i div 10:6);
+end;
+end;
 end.
