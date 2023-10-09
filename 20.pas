84517898ecc2a5b57c08310664398e444c9a6e19
Laptya <144488450+Laptya@users.noreply.github.com>
2023-10-09 22:42:17 +0300

    Update 20.pas

--- 20.pas
+++ 20.pas
@@ -1,7 +1,8 @@
-﻿program zad20;
 var i,j,n,n1,n2:integer;
 begin
+write('Введите число в десятичной системе счисления ');
 readln(n);
+write('Введите в какую систему счислению перевести число');
 readln(j);
 n1:=n;  
 n2:=0;
@@ -9,5 +10,5 @@ i:=1;
 repeat 
   begin n2:=n2+(n1 mod j)*i; n1:=(n1 div j); i:=i*10; end
 until n1 = 0 ;
-write(n2);
+write('Число станет равно ',n2);
 end.
