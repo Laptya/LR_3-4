a48250af6db35000d4573de9c9a1ebaa6926f6a8
Laptya <144488450+Laptya@users.noreply.github.com>
2023-10-09 22:29:51 +0300

    Update 15.pas

--- 15.pas
+++ 15.pas
@@ -1,11 +1,11 @@
 var i,n,count,sum,proiz:integer;
 begin
-write('Введите число');
+write('Введите число ');
   readln(n);
-  i:= 10;
-  count:=1;
-  sum:=n mod 10;
-  proiz:=n mod 10;
+  i:=1;
+  count:=0;
+  sum:=0;
+  proiz:=1;
   repeat 
       if n div i <> 0
       then begin count:= count+1; sum:=n div i mod 10 + sum; proiz:= n div i mod 10 * proiz; i:=i*10; end
