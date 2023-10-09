fa88c470d6b3b7016a550915698d6a9bc246b05d
Laptya <144488450+Laptya@users.noreply.github.com>
2023-10-09 18:00:25 +0300

    Update 9.pas

--- 9.pas
+++ 9.pas
@@ -1,14 +1,15 @@
-﻿
-var i,k,n:integer;
+var k,n:integer;
 sum:real;
 begin
+write('Введите n ');
 readln(n);
 sum:=1;
 k:=1;
+write('Произведение чисел 1*1/2*1/3*...1/n равно ');
 while k-1<>n do
   begin
   sum:=sum*(1/k);
   k:= k+1;
   end;
-print(sum);
+writeln(sum:0:5);
   end.
