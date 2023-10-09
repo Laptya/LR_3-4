b0f3d63593e64db8aea8e674eed01bb4b129700f
Laptya <144488450+Laptya@users.noreply.github.com>
2023-10-09 22:34:31 +0300

    Update 16.pas

--- 16.pas
+++ 16.pas
@@ -1,11 +1,12 @@
-﻿program zad16;
 var i,n:integer;
 z:real;
 begin
   i:=2;
+  write('Введите n ');
   readln(n);
+  write(n,' = ');
 repeat  
-if n mod i = 0 then begin n:=n div i; writeln(i); i:=2; end
+if n mod i = 0 then begin n:=n div i; if n <> 1 then write(i,' * ') else write(i); i:=2; end
     else i:=i+1;
 until n=1;  
 end.
