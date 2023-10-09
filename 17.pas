0ec3211ff22982fb34fdd6a13982b7c5174256ef
Laptya <144488450+Laptya@users.noreply.github.com>
2023-10-09 22:37:26 +0300

    Update 17.pas

--- 17.pas
+++ 17.pas
@@ -1,9 +1,10 @@
-﻿program zad17;
 var bakt,min,i:integer;
 begin
+  write('Введите количество бактерий ');
   readln(bakt);
+  write('Введите количество минут ');
   readln(min);
   repeat if min <> 0 then begin bakt:=bakt*2; min:=min-1; end
   until min <=0;
-  writeln(bakt);
+  writeln('Получится ',bakt,' бактерий');
 end.
