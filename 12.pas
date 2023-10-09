ecd6bab0b7e1fc604d4708a7f76bbca7bb264cb1
Laptya <144488450+Laptya@users.noreply.github.com>
2023-10-09 19:39:11 +0300

    Update 12.pas

--- 12.pas
+++ 12.pas
@@ -3,14 +3,13 @@ begin
   sum:=0;
   i:=1;
   proizvedenie:=1;
+write('Задайте диапазон чисел ');
 readln(n);
 while n<>i-1 do
   begin
  if i mod 2 = 1 then sum:=sum+i else proizvedenie:=proizvedenie*i;
 i:=i+1;
 end;
-writeln(sum);
-writeln(proizvedenie);
+writeln('Cумма чисел диапазона = ',sum);
+writeln('Произведение чисел диапазона = ',proizvedenie);
 end.
-// 1+3+5+7+9=25
-// 2*4*6*8*10 = 3840
